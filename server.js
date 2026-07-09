1d350119e36aa74af8646cd7616a3ca416143316
fatimaAhmed26 <fatima.bh2248@gmail.com>
2026-07-09 12:14:01 +0300

    adding modles folder

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
+const dns = require("node:dns");
+
+dns.setServers(["8.8.8.8", "1.1.1.1"])
+const dotenv =require('dotenv').config() //making .env file available 
 const express =require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 const app = express()
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on('connected',() =>{
+console.log(`connected to MongoDB ${mongoose.connection.name}🥭`)
+})
+
 app.use(morgan('div'))
 
 app.get('/' , async (req ,res)=>{
