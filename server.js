f3b04287d597b0aa984dbd6c7773f60842feffac
fatimaAhmed26 <fatima.bh2248@gmail.com>
2026-07-09 10:01:26 +0300

    creating the views and the gitignore

---
+++ server.js
@@ -0,0 +1,12 @@
+const express =require('express')
+const morgan = require('morgan')
+const app = express()
+app.use(morgan('div'))
+
+app.get('/' , async (req ,res)=>{
+res.send('hello')
+})
+
+app.listen(3000, ()=>{
+console.log('port 3000')
+})
